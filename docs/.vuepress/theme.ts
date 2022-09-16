241a5c6f13af54fd2e82e63a26b3b3c1536a5c2e
myyerrol <myyerrol@126.com>
2022-09-16 23:06:18 +0800

    doc: modify theme's config

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -3,102 +3,50 @@ import * as navbar from "./navbar/index.js";
 import * as sidebar from "./sidebar/index.js";
 
 export default hopeTheme({
-  hostname: "https://vuepress-theme-hope-v2-demo.mrhope.site",
+    hostname: "https://myyerrol.github.io/ysyx-website",
+    author: {
+        name: "myyerrol",
+        url: "https://myyerrol.github.io",
+    },
+    locales: {
+        "/": {
+            navbar: navbar.en,
+            sidebar: sidebar.en,
+            footer: "Default footer",
+            displayFooter: true
+        },
+        "/zh/": {
+            navbar: navbar.zh,
+            sidebar: sidebar.zh,
+            footer: "默认页脚",
+            displayFooter: true
+        }
+    },
 
-  author: {
-    name: "Mr.Hope",
-    url: "https://mrhope.site",
-  },
+    navbarIcon: true,
+    navbarLayout: {
+        left: ["Brand"],
+        center: ["Links"],
+        right: ["Language", "Repo", "Outlook", "Search"]
+    },
+    logo: "/logo.svg",
+    logoDark: "/logo.svg",
+    repo: "myyerrol/ysyx_website",
+    repoDisplay: true,
+    navbarAutoHide: "mobile",
+    hideSiteNameonMobile: true,
+
+
+
+
 
   iconAssets: "iconfont",
 
-  logo: "/logo.svg",
-
-  repo: "vuepress-theme-hope/vuepress-theme-hope",
 
   docsDir: "demo/src",
 
   pageInfo: ["Author", "Original", "Date", "Category", "Tag", "ReadingTime"],
 
-  blog: {
-    medias: {
-      Baidu: "https://example.com",
-      Bitbucket: "https://example.com",
-      Dingding: "https://example.com",
-      Discord: "https://example.com",
-      Dribbble: "https://example.com",
-      Email: "https://example.com",
-      Evernote: "https://example.com",
-      Facebook: "https://example.com",
-      Flipboard: "https://example.com",
-      Gitee: "https://example.com",
-      GitHub: "https://example.com",
-      Gitlab: "https://example.com",
-      Gmail: "https://example.com",
-      Instagram: "https://example.com",
-      Lines: "https://example.com",
-      Linkedin: "https://example.com",
-      Pinterest: "https://example.com",
-      Pocket: "https://example.com",
-      QQ: "https://example.com",
-      Qzone: "https://example.com",
-      Reddit: "https://example.com",
-      Rss: "https://example.com",
-      Steam: "https://example.com",
-      Twitter: "https://example.com",
-      Wechat: "https://example.com",
-      Weibo: "https://example.com",
-      Whatsapp: "https://example.com",
-      Youtube: "https://example.com",
-      Zhihu: "https://example.com",
-    },
-  },
-
-  locales: {
-    "/": {
-      // navbar
-      navbar: navbar.en,
-
-      // sidebar
-      sidebar: sidebar.en,
-
-      footer: "Default footer",
-
-      displayFooter: true,
-
-      blog: {
-        description: "A FrontEnd programmer",
-        intro: "/intro.html",
-      },
-    },
-
-    /**
-     * Chinese locale config
-     */
-    "/zh/": {
-      // navbar
-      navbar: navbar.zh,
-
-      // sidebar
-      sidebar: sidebar.zh,
-
-      footer: "默认页脚",
-
-      displayFooter: true,
-
-      blog: {
-        description: "一个前端开发者",
-        intro: "/zh/intro.html",
-      },
-    },
-  },
-
-  encrypt: {
-    config: {
-      "/guide/encrypt.html": ["1234"],
-      "/zh/guide/encrypt.html": ["1234"],
-    },
-  },
 
   plugins: {
     blog: {
